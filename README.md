7f330c1d083a73ba9e02625cf82ad2f3ab773023
Ben <benedict_sun@dlsu.edu.ph>
2022-12-17 23:11:12 +0800

    Migrate commits from forked repo

---
+++ README.md
@@ -0,0 +1,13 @@
+## Developing A Game Recommendation Engine using Social, Interaction and Sentiment Features on a Graph Convolutional Network
+
+> Authors: Pete Choi, Wilfred Go, Benedict Sun, James Sy
+
+Credits to Yang et al.'s SCGRec Model, which was forked from their repository found [here](https://github.com/YangLiangwei/SCGRec).
+
+## Description
+
+In partial fulfillment of the requirements for THS-ST, this project tests the inclusion of additional social, interaction, and sentiment features on the Social-aware Contextualiazed Graph Neural Network proposed by Yang et al (2022).
+
+## How to run
+
+python main.py
---
+++ utils/parser.py
@@ -0,0 +1,42 @@
+import argparse
+
+"""
+    Argparser allows user to pass in parameters from command line. If no parameter is passed from cmd, default value is used.
+    Can access arguments passed as args.argname or args[argname]
+"""
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', default = 'Ciao', type = str,
+                        help = 'Dataset to use')
+    parser.add_argument('--train_percent', default = 0.8, type = float,
+                        help = 'training_percent')
+    parser.add_argument('--embed_size', default = 32, type = int,
+                        help = 'embedding size for all layer')
+    parser.add_argument('--lr', default = 0.03, type = float,
+                        help = 'learning rate')
+    parser.add_argument('--model', default = 'RGCN', type = str,
+                        help = 'model selection')
+    parser.add_argument('--epoch', default = 1000, type = int,
+                        help = 'epoch number')
+    parser.add_argument('--early_stop', default = 10, type = int,
+                        help = 'early_stop validation')
+    parser.add_argument('--batch_size', default = 1024, type = int,
+                        help = 'batch size')
+    parser.add_argument('--layers', default = 1, type = int,
+                        help = 'layer number')
+    parser.add_argument('--gpu', default = -1, type = int,
+                        help = '-1 for cpu, 0 for gpu:0')
+    parser.add_argument('--k', default = [5, 10, 20], type = list,
+                        help = 'negative sampler number for each node')
+    # TODO: Determine either w_context or w_self
+    parser.add_argument('--g', default = 0.1, type = float,
+                        help = 'hyper-parameter for aggregation weight')
+    parser.add_argument('--social_g', default = 0.1, type = float,
+                        help = 'hyper-parameter for social aggregation weight')
+    # TODO: Determine either w_context or w_self
+    parser.add_argument('--item_g', default = 0.1, type = float,
+                        help = 'hyper-parameter for aggregation weight')
+
+    args = parser.parse_args()
+    return args
+
